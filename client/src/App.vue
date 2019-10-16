b75006be1157b759a319084e6ec2364534fdf6c4
jianglibo <781630130@qq.com>
2019-10-16 18:32:30 +0800

    初始化与客户端

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Vue } from "vue-property-decorator";
+export default class App extends Vue {}
+</script>
+<style>
+.slot {
+  width: 100%;
+  height: 100vh;
+}
+</style>
